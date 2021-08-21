7cf72bdaaaaf757689f6469ebd472411c929dace
Jewell, Charlie <Charles.Jewell@uk.dk.com>
2021-08-21 21:57:01 +0100

    added fonts

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,26 @@
 import React from 'react';
+import Head from 'next/head';
+
 
 const Home = () => {
   return (
-    <h1>Home</h1>
+    <React.Fragment>
+      <Head>
+        <link
+          rel='preload'
+          href='/fonts/avenir-next-bold.woff2'
+          as='font'
+          crossOrigin=''
+        />
+        <link
+          rel='preload'
+          href='/fonts/avenir-next-regular.woff2'
+          as='font'
+          crossOrigin=''
+        />
+      </Head>
+      <h1>Home</h1>
+    </React.Fragment>
   );
 };
 
