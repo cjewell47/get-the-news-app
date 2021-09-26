2651bd3f42a5b81b97e3218d07029e5fb2c40c91
Jewell, Charlie <Charles.Jewell@uk.dk.com>
2021-09-26 15:13:19 +0100

    more tests passing

--- __tests__/[country].test.tsx
+++ __tests__/[country].test.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import {render} from '@testing-library/react';
+import {fireEvent, render, screen} from '@testing-library/react';
 import '@testing-library/jest-dom';
 import Country from '../pages/[country]';
 import countryData from '../mock-data/countries.json';
@@ -29,4 +29,43 @@ describe('Country', () => {
     const length = news.length;
     expect(articles).toHaveLength(length);
   });
+
+  it('articles should have a headline', () => {
+    const {container} = render(
+      <Country error={error} news={news} country={country} />,
+    );
+
+    const title = news[0].title;
+
+    const heading = screen.getByText(title);
+    
+    expect(heading).toBeInTheDocument();
+  });
+
+  it('renders a list of 8 checkboxes', () => {
+    const {container} = render(
+      <Country error={error} news={news} country={country} />,
+    );
+
+    const filters = container.querySelectorAll('input[type="checkbox"]');
+    const length = Array.from(new Set(news.map(n => n.source.name))).length;
+    expect(filters).toHaveLength(length);
+  });
+
+
+  it('clicking a checkbox renders only articles with that source', () => {
+    const {container} = render(
+      <Country error={error} news={news} country={country} />,
+    );
+    
+
+    const thisSource = news[0].source.name
+
+    fireEvent.click(document.querySelector(`label[for="${thisSource}"]`))
+
+    const articles = container.querySelectorAll('li');
+    const length = news.filter(n => n.source.name === thisSource).length;
+    expect(articles).toHaveLength(length);
+  });
+
 });
